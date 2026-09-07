e515249faf4b82177e4bdcef1d3be76eb23ad15b
Ethan Gat <100442275+Anonymoose725@users.noreply.github.com>
2026-09-07 17:15:29 -0400

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       // flatten array and do a regular binary search
+       // run a single binary search from index 0 to (rows * cols - 1)
+       int m = matrix.length;
+       int n = matrix[0].length;
+       int left = 0;
+       int right = m * n - 1;
+
+       while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] == target) return true;
+
+            else if (target > matrix[row][col]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+       }
+
+       return false;
+    }
+}
